91eeb4b0dd47b124c12a553eee4fbe710ee8e8d5
Timi <timilehinoladapo0@gmail.com>
2024-07-22 05:27:44 +0100

    bug: sticky bug

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,105 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Container from "./Container";
 import Image from "next/image";
 import demoImage from "../../public/assets/images/1.jpg";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 
 function Menu() {
+	const [currentSection, setCurrentSection] = useState<string>("#bread");
+	const params = useParams();
+	useEffect(() => {
+		setCurrentSection(window.location.hash);
+	}, [params]);
+
 	return (
-		<section className="grid grid-cols-1 lg:grid-cols-[60%,1fr] lg:gap-8">
-			<div className="lg:sticky lg:top-0">
-				<Image
-					src={demoImage}
-					className="h-[100vh] w-full object-cover lg:sticky lg:top-0"
-					alt="demo image"
-				/>
-			</div>
-			<div>Categories Section</div>
-		</section>
+		<main className="mt-6 mb-10 flex  sticky top-0  gap-8 lg:gap-6">
+			<section className="w-[60%]   border border-green-700">
+				<div className="bg-pink-500 h-[100px] sticky   top-0">1</div>
+			</section>
+			<section className="">
+				<div className="h-[1000px]">12</div>
+			</section>
+		</main>
 	);
 }
 
 export default Menu;
+{
+	/* // <main className="grid grid-cols-1 lg:grid-cols-[60%,1fr] lg:gap-8"> */
+}
+// 	<section className="sticky">
+// 		<div className="md:h-[calc(100vh-50px)]  w-full bg-red-500 ">
+// 			{/* <div className="bg-black/80 absolute z-20 h-full" /> */}
+// 			{/* <Image
+// 				src={demoImage}
+// 				className="w-full absolute h-full object-cover"
+// 				alt="cake image"
+// 			/> */}
+// 		</div>
+// 	</section>
+// 	<section>
+// 		<nav className="mx-auto">
+// 			<ul className="flex items-center justify-center flex-wrap text-lg   text-white/70 gap-8 my-8">
+// 				<li>
+// 					<Link
+// 						href={"#bread"}
+// 						className={`py-1 border-b ease-in-out duration-100 hover:border-white/70
+//         ${
+// 							currentSection === "#bread"
+// 								? "border-white/70"
+// 								: "border-white/0"
+// 						}`}
+// 					>
+// 						Bread
+// 					</Link>
+// 				</li>
+// 				<li>
+// 					<Link
+// 						href={"#cakes"}
+// 						className={`py-1 border-b ease-in-out duration-100 hover:border-white/70
+//         ${
+// 							currentSection === "#cakes"
+// 								? "border-white/70"
+// 								: "border-white/0"
+// 						}`}
+// 					>
+// 						Cakes
+// 					</Link>
+// 				</li>
+// 				<li>
+// 					<Link
+// 						href={"#drinks"}
+// 						className={`py-1 border-b ease-in-out duration-100 hover:border-white/70
+//         ${
+// 							currentSection === "#drinks"
+// 								? "border-white/70"
+// 								: "border-white/0"
+// 						}`}
+// 					>
+// 						Drinks
+// 					</Link>
+// 				</li>
+
+// 				<li>
+// 					<Link
+// 						href={"#specials"}
+// 						className={`py-1 border-b ease-in-out duration-100 hover:border-white/70
+//         ${
+// 							currentSection === "#specials"
+// 								? "border-white/70"
+// 								: "border-white/0"
+// 						}`}
+// 					>
+// 						Specials
+// 					</Link>
+// 				</li>
+// 			</ul>
+// 		</nav>
+
+// 		<div id="bread">
+// 			<p>This is our bread container o</p>
+// 		</div>
+// 	</section>
+// </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,100 +5,106 @@ import React, { useState } from "react";
 import { MdOutlinePhoneInTalk } from "react-icons/md";
 
 const Navbar = () => {
-  const pathname = usePathname();
-  const [isOpen, setIsOpen] = useState(false);
+	const pathname = usePathname();
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
 
-  return (
-    <header className={`px-5 md:px-7 md:py-4 ${isOpen ? 'bg-customBlack/90 md:bg-customBlack' : 'bg-customBlack'}
-     bg-customBlack items-center sticky top-0 z-[100] h-[65px] md:h-auto`}>
-      <div className="flex items-center h-full">
-        <Link
-          href="/"
-          className="tracking-wider lora-heading capitalize text-3xl md:text-4xl"
-        >
-          CHOW
-        </Link>
+	return (
+		<header
+			className={`px-5 md:px-7 md:py-4 ${
+				isOpen ? "bg-customBlack/90 md:bg-customBlack" : "bg-customBlack"
+			}
+     bg-customBlack items-center sticky top-0 z-[100] h-[65px] md:h-auto`}
+		>
+			<div className="flex items-center h-full">
+				<Link
+					href="/"
+					className="tracking-wider lora-heading capitalize text-3xl md:text-4xl"
+				>
+					CHOW
+				</Link>
 
-        <section
-          className={`grow items-center absolute top-[100%] flex md:static flex-col md:flex-row ease-in-out duration-200  
-md:h-auto ${isOpen ? 'h-[calc(100vh-65px)]' : 'h-0'} gap-6 md:gap-0 overflow-hidden
+				<section
+					className={`grow items-center absolute top-[100%] flex md:static flex-col md:flex-row ease-in-out duration-200  
+md:h-auto ${
+						isOpen ? "h-[calc(100vh-65px)]" : "h-0"
+					} gap-6 md:gap-0 overflow-hidden
  bg-customBlack/90 w-full left-0 `}
-        >
-          <div className="flex flex-col text-sm md:ml-7 text-center mt-5 md:mt-0 md:text-start">
-            <p className="text-white/70">Mon - Sun</p>
-            <p>08:00 - 20:00</p>
-          </div>
+				>
+					<div className="flex flex-col text-sm md:ml-7 text-center mt-5 md:mt-0 md:text-start">
+						<p className="text-white/70">Mon - Sun</p>
+						<p>08:00 - 20:00</p>
+					</div>
 
-          <nav className="mx-auto">
-            <ul className="flex gap-4 md:gap-12 flex-col md:flex-row text-center md:text-start">
-              <li>
-                <Link
-                  href={"/"}
-                  className={`py-1 border-b ease-in-out duration-100 hover:border-white 
+					<nav className="mx-auto">
+						<ul className="flex gap-4 md:gap-12 flex-col md:flex-row text-center md:text-start">
+							<li>
+								<Link
+									href={"/"}
+									className={`py-1 border-b ease-in-out duration-100 hover:border-white 
                 ${pathname === "/" ? "border-white" : "border-white/0"}`}
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/menu"}
-                  className={`py-1 border-b ease-in-out duration-100 hover:border-white 
+								>
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link
+									href={"/menu"}
+									className={`py-1 border-b ease-in-out duration-100 hover:border-white 
                 ${pathname === "/menu" ? "border-white" : "border-white/0"}`}
-                >
-                  Menu
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/contact"}
-                  className={`py-1 border-b ease-in-out duration-100 hover:border-white 
+								>
+									Menu
+								</Link>
+							</li>
+							<li>
+								<Link
+									href={"/contact"}
+									className={`py-1 border-b ease-in-out duration-100 hover:border-white 
                 ${pathname === "/contact" ? "border-white" : "border-white/0"}`}
-                >
-                  Contacts
-                </Link>
-              </li>
-            </ul>
-          </nav>
+								>
+									Contacts
+								</Link>
+							</li>
+						</ul>
+					</nav>
 
-          <a
-            href="tel:+"
-            className="flex gap-3 items-center tracking-wider text-white"
-          >
-            <MdOutlinePhoneInTalk size={24} /> 08111108992
-          </a>
-          <button className="p-2 px-4 bg-white text-black tracking-wide md:ml-7">
-            Order Now
-          </button>
-        </section>
+					<a
+						href="tel:+"
+						className="flex gap-3 items-center tracking-wider text-white"
+					>
+						<MdOutlinePhoneInTalk size={24} /> 08111108992
+					</a>
+					<button className="p-2 px-4 bg-white text-black tracking-wide md:ml-7">
+						Order Now
+					</button>
+				</section>
 
-        <button
-          className="flex md:hidden ml-auto flex-col items-center justify-center w-10 h-10 space-y-1"
-          onClick={toggleMenu}
-        >
-          <span
-            className={`block w-6 h-0.5 bg-white transform transition-transform duration-300 ease-in-out ${
-              isOpen ? "rotate-45 translate-y-2" : ""
-            }`}
-          ></span>
-          <span
-            className={`block w-6 h-0.5 bg-white transition-opacity duration-300 ease-in-out ${
-              isOpen ? "opacity-0" : ""
-            }`}
-          ></span>
-          <span
-            className={`block w-6 h-0.5 bg-white transform transition-transform duration-300 ease-in-out ${
-              isOpen ? "-rotate-45 -translate-y-1" : ""
-            }`}
-          ></span>
-        </button>
-      </div>
-    </header>
-  );
+				<button
+					className="flex md:hidden ml-auto flex-col items-center justify-center w-10 h-10 space-y-1"
+					onClick={toggleMenu}
+				>
+					<span
+						className={`block w-6 h-0.5 bg-white transform transition-transform duration-300 ease-in-out ${
+							isOpen ? "rotate-45 translate-y-2" : ""
+						}`}
+					></span>
+					<span
+						className={`block w-6 h-0.5 bg-white transition-opacity duration-300 ease-in-out ${
+							isOpen ? "opacity-0" : ""
+						}`}
+					></span>
+					<span
+						className={`block w-6 h-0.5 bg-white transform transition-transform duration-300 ease-in-out ${
+							isOpen ? "-rotate-45 -translate-y-1" : ""
+						}`}
+					></span>
+				</button>
+			</div>
+		</header>
+	);
 };
 
 export default Navbar;
